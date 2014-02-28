eb7dcbed6276efa55a516b970deb933ad543d15d
Philippe Caseiro <pcaseiro@cadoles.com>
2014-02-28 22:57:06 +0100

    Cleaning code

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ var fs = require('fs');
 var path = require('path');
 
 function ls(params, cb) {
-    var app_name = params[0];
-    var root = path.join(this.config.get('apps:dir'), app_name, params[1]);
+    var appName = params[0];
+    var root = path.join(this.config.get('apps:dir'), appName, params[1]);
 
     fs.readdir(root, cb);
-}
+  }
 
-function ls_load(Opts, cb) {
-    console.log("TOTO");
+function lsLoad(opts, cb) {
     // Expose methods to Supervisor over RPC
     this.rpc.expose('dir', 'ls', ls.bind(this));
-    console.log("TOTO");
 
     // Inject client.js to the client
     this.plugins.injectClientSide(__dirname + '/client.js' );
     return cb();
-}
+  }
 
 module.exports = {
-    load: ls_load
-}
+    load: lsLoad
+  };
